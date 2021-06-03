0067f02c5a2a438da11dd9fa7c6c2dd421b79b5c
Carlos Aroca <carlosarocac@gmail.com>
2021-06-02 19:24:57 -0500

    Feat: add Custom component

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,7 +1,9 @@
 import { wrapFunctional } from './utils'
 
+export { default as Custom } from '../..\\components\\Custom.vue'
 export { default as Logo } from '../..\\components\\Logo.vue'
 export { default as Navbar } from '../..\\components\\Navbar.vue'
 
+export const LazyCustom = import('../..\\components\\Custom.vue' /* webpackChunkName: "components/custom" */).then(c => wrapFunctional(c.default || c))
 export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
 export const LazyNavbar = import('../..\\components\\Navbar.vue' /* webpackChunkName: "components/navbar" */).then(c => wrapFunctional(c.default || c))
